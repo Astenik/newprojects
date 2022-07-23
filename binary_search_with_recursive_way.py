8d4484d8cd2a310f05627c6e9699461c54da07ae
Astenik <106985461+Astenik@users.noreply.github.com>
2022-07-23 18:40:16 +0400

    Create binary_search_with_recursive_way.py

---
+++ binary_search_with_recursive_way.py
@@ -0,0 +1,20 @@
+def binary_search_rec(lst, key, start, end):
+       '''this function finds key in the list if it is else return -1
+       my function have 4 argument where the first argument is the list
+       the second argument is the key the third argument is the start of my list
+       and the 4-rd argument is the end of my list.'''
+       if len(lst) == 0:
+            return -1 
+       mid = (start + end) // 2
+       if lst[mid] == key:
+            return mid
+       if key > lst[mid]:
+            s = mid + 1
+            return binary_search_rec(lst, key, s, end) 
+       else:
+            e = mid - 1
+            return binary_search_rec(lst, key, start, e)
+lst = [12, 45, 89, 90, 5363]
+key = int(input("insert the key: "))
+end = int(input("insert length of your list: "))
+print(f"{key}'s position in the {lst} is: {binary_search_rec(lst, key, 0, end)}")
